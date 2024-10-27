1a6c655321068c3b97c7b0f5de3c84fff6cb04fc
gregfc95 <josegfc95@gmail.com>
2024-10-27 16:25:02 -0300

    main structure of the project

---
+++ plataforma-frontend/src/App.js
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/NavBarComponent';
+import { JuegoPage, JuegoDetailPage, LoginPage, RegistroPage } from './pages';
+import FooterComponent from './components/FooterComponent';
+//import { useState, useEffect }  from 'react';
+
+//TODO
+//Toastify 
+const App = () => {
+  return (
+    <>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<JuegoPage />} />
+        <Route path="/juego/:id" element={<JuegoDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/registro" element={<RegistroPage />} />
+      </Routes> 
+
+    <FooterComponent name="Jose Gregorio Fernandez Campos" group="6" />
+    </>
+  );
+}
+
+export default App;
