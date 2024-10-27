089ae713c82616b3333d817d2502f285f7102d06
gregfc95 <josegfc95@gmail.com>
2024-10-27 20:42:58 -0300

    login and signup pages with styles

--- plataforma-frontend/src/pages/registro/LoginPage.js
+++ plataforma-frontend/src/pages/registro/LoginPage.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
+import { validateFields} from "../../utils/validationUtils";
+import { FaUser, FaLock } from "react-icons/fa6";
+import "../../assets/styles/login.css";
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-export default LoginPage
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Evita que la página se recargue al enviar el formulario
+    
+    // Validacion username and password
+    const fieldsError = validateFields(username, password);
+
+    if (fieldsError) {
+      setErrorMessage(fieldsError);
+      return;
+    }
+
+    setErrorMessage(''); // Limpiar el mensaje de error
+    console.log("Form submitted:", { username, password });
+  };
+
+
+  return (
+    <div className="login-page">
+      <form onSubmit={handleSubmit}>
+        <h2>Iniciar Sesion</h2>
+        <div className="input-box">
+          <input type="text" 
+          placeholder="Nombre de Usuario" 
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-box">
+          <input type="password" 
+          value={password} onChange={(e) => setPassword(e.target.value)} 
+          placeholder="Contraseña" 
+          required />
+          <FaLock className="icon" />
+        </div>
+
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+        <div className="remember-forgot">
+          <label>
+            <input type="checkbox" /> Recordarme
+          </label>
+        </div>
+        <button type="submit">Login</button>
+
+        <div className="register-link">
+          <p>
+            ¿No tienes una cuenta? <a href="/registro">Registrate</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
--- plataforma-frontend/src/pages/registro/RegistroPage.js
+++ plataforma-frontend/src/pages/registro/RegistroPage.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useState } from "react";
+import { validateFields, validateUsername, validatePassword } from "../../utils/validationUtils";
+import { FaUser, FaLock } from "react-icons/fa6";
+import "../../assets/styles/login.css";
 
 const RegistroPage = () => {
-  return (
-    <div>RegistroPage</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-export default RegistroPage
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Evita que la página se recargue al enviar el formulario
+    
+    // Validacion username and password
+    const usernameError = validateUsername(username);
+    const passwordError = validatePassword(password);
+    const fieldsError = validateFields(username, password);
+
+    if (fieldsError) {
+      setErrorMessage(fieldsError);
+      return;
+    }
+
+    if (usernameError) {
+      setErrorMessage(usernameError);
+      return;
+    }
+    
+    if (passwordError) {
+      setErrorMessage(passwordError);
+      return;
+    }
+
+    setErrorMessage(''); // Limpiar el mensaje de error
+    console.log("Form submitted:", { username, password });
+  };
+
+
+  return (
+    <div className="signup-page">
+      <form onSubmit={handleSubmit}>
+        <h2>Registrate!</h2>
+        <div className="input-box">
+          <input type="text" 
+          placeholder="Usuario" 
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-box">
+          <input type="password" 
+          value={password} onChange={(e) => setPassword(e.target.value)} 
+          placeholder="Password" 
+          required />
+          <FaLock className="icon" />
+        </div>
+
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+        <button type="submit">Sign up</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegistroPage;
